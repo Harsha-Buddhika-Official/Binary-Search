17938ea98fdde868620e4e59641ca7bf82c301a5
Harsha Buddhika <59781888+Harsha-Buddhika-Official@users.noreply.github.com>
2025-08-04 21:51:59 +0530

    Update binarySearch.java

--- binarySearch.java
+++ binarySearch.java
@@ -1,6 +1,19 @@
 import java.util.Scanner;
 
 public class binarySearch{
+    public static int Sort(int[] arr){
+        for(int i=0; i<arr.length-1; i++){
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[i]>arr[j]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return 0;
+    }
+
     public static int Search(int[] arr, int low, int high, int target) {
         while (low <= high) {
             int mid = low + (high - low) / 2;
@@ -18,15 +31,24 @@ public class binarySearch{
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the array size:");
         int size = sc.nextInt();
+
         int[] arr = new int[size];
         System.out.print("Enter the elements of the array (space-separated): ");
         for (int i = 0; i < size; i++) {
             arr[i] = sc.nextInt();
         }
-        System.out.print("Enter the target element: ");
+
+        Sort(arr);
+        System.out.println("Sorted array: ");
+        for (int i = 0; i < size; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.print("\nEnter the target element: ");
         int target = sc.nextInt();
         sc.close();
-        int result = Search(arr, 0, arr.length -1, target);
+
+        int result = Search(arr, 0, arr.length - 1, target);
         System.out.println("Element found at index: " + result);
     }
 }
